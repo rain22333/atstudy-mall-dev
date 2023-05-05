224fcd09666b0ef4988904f5d967199f5b44b42d
atstudy <rain12home@gmail.com>
2023-05-05 11:00:01 +0800

    首次提交，框架搭建完毕

---
+++ src/main/java/com/atstudy/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.atstudy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/","/index"})
+public class IndexController {
+
+    @RequestMapping({"/","/index"})
+    public String index(){
+        return "index";
+    }
+
+
+    @RequestMapping("/home")
+    public String home(){
+        return "home";
+    }
+
+
+}
